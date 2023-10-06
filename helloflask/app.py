ea7d3469f5e7c5f2f236bd28697f796a17c1656e
Abdullah-Nadir <abdullahnadir495@gmail.com>
2023-10-05 21:39:00 -0700

    Add files via upload

---
+++ helloflask/app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        color = request.form.get("color")
+        return render_template("color.html", color=color)
+    return render_template("index.html")
